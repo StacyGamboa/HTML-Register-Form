7cffd61ea4a23d3d1de0034039b4ef98f52b99df
StacyGamboa <36346713+StacyGamboa@users.noreply.github.com>
2018-02-12 14:57:25 -0600

    Create register.php

---
+++ register.php
@@ -0,0 +1,68 @@
+<?php
+    header('Content-type: application/json');
+    $status = array(
+        'status'=>'success',
+        'message'=>'Thank you! One of our agents will contact you soon.'
+    );
+
+    $debug_log = "./debugginglog.txt";
+    $currContent = file_get_contents($debug_log);
+    file_put_contents($debug_log, "Trying to send email ... \n");
+
+    $subject = 'Confirmation of registration';
+    $fname = @trim(stripslashes($_POST['fname']));
+    $lname = @trim(stripslashes($_POST['lname']));
+    $email = @trim(stripslashes($_POST['email']));
+    $cemail = @trim(stripslashes($_POST['cemail']));
+    $password = @trim(stripslashes($_POST['password']));
+    $cpassword = @trim(stripslashes($_POST['cpassword']));
+    $country = @trim(stripslashes($_POST['country']));
+    $number = @trim(stripslashes($_POST['phone']));
+    $area = @trim(stripslashes($_POST['area']));
+    $details = @trim(stripslashes($_POST['details']));
+    $nameoncard = @trim(stripslashes($_POST['nameoncard']));
+    $ccnumber = @trim(stripslashes($_POST['ccnumber']));
+    $expirymonth = @trim(stripslashes($_POST['expiry-month']));
+    $expiryyear = @trim(stripslashes($_POST['expiry-year']));
+    $ccv = @trim(stripslashes($_POST['ccv']));
+    $plan = $_POST['plan'];
+    $agreeTerms = @trim(stripslashes(implode(', ', $_POST['agreeTerms'])));
+
+    $email_from = $email;
+    $email_to = 'info@companyname'; //replace with your email
+
+    $body =
+        'Name: ' . $fname . "\n\n" .
+        'Last Name: ' . $lname . "\n\n" .
+        'Email: ' . $email . "\n\n" .
+        'Confirm Email: ' . $cemail . "\n\n" .
+        'Password: ' . $password . "\n\n" .
+        'Confirm Pass: ' . $cpassword . "\n\n" .
+        'Country: ' . $country . "\n\n" .
+        'Number: ' . $number . "\n\n" .
+        'Field: ' . $area . "\n\n" .
+        'Details: ' . $details . "\n\n" .
+        'Name on card: ' . $nameoncard . "\n\n" .
+        'Card number: ' . $ccnumber . "\n\n" .
+        'Expiration Date: ' . $expirymonth . "/" . $expiryyear . "\n\n" .
+        'Security code: ' . $ccv . "\n\n" .
+        'Agree Terms: ' . $agreeTerms . "\n\n" .
+        'Plan: ' . $plan . "\n\n";
+
+
+    $subjectEmailFrom = $email_to;
+    @mail($email_from, $subject.' '.$subjectEmailFrom, 'Plan: ' . $plan . "\n\n" .'Thank you! One of our agents will contact you soon.', 'From: <'.$email_to.'>');
+
+    @mail($email_to, 'A customer has been registered.', $body , 'From: <'.$email_to.'>');
+
+    // Ahora se envía el e-mail usando la función mail() de PHP
+
+
+
+    $currContent = file_get_contents($debug_log);
+    file_put_contents($debug_log, "Response ..." . $success . "\n");
+
+    echo json_encode($status);
+
+die;
+?>
